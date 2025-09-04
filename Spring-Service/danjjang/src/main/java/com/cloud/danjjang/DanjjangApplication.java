a7b74dce81f2e6a7dcc9378d05ad644c05b90693
KimChaeWon12 <kcw030220@naver.com>
2025-09-04 11:23:42 +0900

    🔧 chore: spring 초기 세팅

---
+++ Spring-Service/danjjang/src/main/java/com/cloud/danjjang/DanjjangApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.danjjang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanjjangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanjjangApplication.class, args);
+	}
+
+}
